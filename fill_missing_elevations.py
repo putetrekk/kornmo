0b0bda7c22e288fe5fc67cdd99c51a2d0909d39f
putetrekk <17160920+putetrekk@users.noreply.github.com>
2020-11-08 18:23:00 +0100

    Add get_farmer_coordinates_v2.py

---
+++ fill_missing_elevations.py
@@ -0,0 +1,18 @@
+import pandas
+import requests
+
+if __name__ == '__main__':
+    df = pandas.read_csv('data/farmer_elevation.csv')
+    elevations_by_commune = df.groupby('commune_id').median()
+    elevations_by_commune: pandas.DataFrame = elevations_by_commune['elevation']
+
+    def get_missing_elevation(k: pandas.Series):
+        if k.isnull()['elevation']:
+            print(f"farmer in {k['commune']} is missing elevation!")
+            return elevations_by_commune.loc[k['commune_id']]
+        else:
+            return k['elevation']
+
+
+    df['elevation'] = df.apply(get_missing_elevation, axis=1)
+    df.to_csv('data/farmer_elevation_plus.csv')
